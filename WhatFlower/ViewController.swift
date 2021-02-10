7f0cfe6ed0a4841121ccfea656315d0bddbb5fe3
Bogdan Orzea <bogdan.orzea@gmail.com>
2021-02-10 11:16:27 -0500

    Adds FlowerClassifier and code to take picture from camera

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let imagePicker: UIImagePickerController = UIImagePickerController()
+
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
     }
 
-
+    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let originalImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView.image = originalImage
+        }
+
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
