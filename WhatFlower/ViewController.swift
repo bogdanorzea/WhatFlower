be65f22be60e9dbdab3ebd3091a7f50e9f8eb249
Bogdan Orzea <bogdan.orzea@gmail.com>
2021-02-20 22:56:07 -0500

    Implement ML image processing

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     let imagePicker: UIImagePickerController = UIImagePickerController()
 
     @IBOutlet weak var imageView: UIImageView!
@@ -23,14 +25,42 @@ class ViewController: UIViewController {
     @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
-}
 
-extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let originalImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            guard let ciImage = CIImage(image: originalImage) else {
+                fatalError("Could not convert image to CIImage.")
+            }
+
             imageView.image = originalImage
+
+            detect(flowerImage: ciImage)
         }
 
         picker.dismiss(animated: true, completion: nil)
+    }
+
+    func detect(flowerImage: CIImage) {
+        let config = MLModelConfiguration()
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier(configuration: config).model) else {
+            fatalError("Loading CoreML model failed")
+        }
+
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("CoreML failed to process image")
+            }
+
+            if let firstResult = results.first {
+                self.navigationItem.title = firstResult.identifier.capitalized
+            }
+        }
+
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Failed to perform CoreML request: \(error)")
+        }
     }
 }
