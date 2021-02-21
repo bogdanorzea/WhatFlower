ac850da08c34d7d57cf6c76e7069b5e6624e1f0f
Bogdan Orzea <bogdan.orzea@gmail.com>
2021-02-21 11:45:16 -0500

    Complete Section 24

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 import CoreML
 import Vision
 
+import Alamofire
+import SwiftyJSON
+import SDWebImage
+
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     let imagePicker: UIImagePickerController = UIImagePickerController()
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var extractLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,7 +57,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
 
             if let firstResult = results.first {
-                self.navigationItem.title = firstResult.identifier.capitalized
+                let flowerName = firstResult.identifier
+                self.navigationItem.title = flowerName.capitalized
+
+                self.fetchWikiInfoFor(flowerName)
             }
         }
 
@@ -62,5 +70,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         } catch {
             print("Failed to perform CoreML request: \(error)")
         }
+    }
+
+    func fetchWikiInfoFor(_ titles: String) {
+        let wikipediaURl = "https://en.wikipedia.org/w/api.php"
+
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts|pageimages",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : titles,
+            "indexpageids" : "",
+            "redirects" : "1",
+            "pithumbsize": "500"
+        ]
+
+        AF.request(wikipediaURl, method: .get, parameters: parameters)
+            .responseJSON { response in
+                if response.error == nil, let data = response.data {
+                    guard let json = try? JSON(data: data) else {
+                        print("Could not parse JSON: \(data)")
+                        return
+                    }
+
+                    let pageId = json["query"]["pageids"][0].stringValue
+                    let page = json["query"]["pages"][pageId]
+                    let pageExtract = page["extract"].stringValue
+                    let pageImage = page["thumbnail"]["source"].stringValue
+
+                    self.imageView.sd_setImage(with: URL(string: pageImage))
+                    self.extractLabel.text = pageExtract
+
+                    debugPrint(json)
+                }
+            }
     }
 }
